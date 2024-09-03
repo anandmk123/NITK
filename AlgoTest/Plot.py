47f913e7d965a0b9ad1c9b549133f689fe20c183
anandmk123 <anandmkunni@gmail.com>
2024-09-03 14:19:47 +0530

    sadas

--- AlgoTest/Plot.py
+++ AlgoTest/Plot.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data from the first file
-data1 = np.loadtxt('./Experiments/time 1/RandomQuickPivotFirstTime')
+data1 = np.loadtxt('./Experiments/time 1/DescendQuickPivotFirstTime')
 x1 = data1[:, 0]
 y1 = data1[:, 1]
 
 # Load data from the second file
-data2 = np.loadtxt('./Experiments/time 1/SortedQuickPivotRandomTime')
+data2 = np.loadtxt('./Experiments/time 1/DescendQuickPivotRandomTime')
 x2 = data2[:, 0]
 y2 = data2[:, 1]
 
-data3 = np.loadtxt('./Experiments/time 3/SortedQuickPivotMedianTime')
+data3 = np.loadtxt('./Experiments/time 3/DescendQuickPivotMedianTime')
 x3 = data3[:, 0]
 y3 = data3[:, 1]
 
@@ -19,7 +19,7 @@ y3 = data3[:, 1]
 plt.figure(figsize=(10, 6))
 
 # Plot the first dataset
-plt.plot(x1, y1, marker='o', linestyle='-', color='g', label='Pivot First')
+# plt.plot(x1, y1, marker='o', linestyle='-', color='g', label='Pivot First')
 
 # Plot the second dataset
 plt.plot(x2, y2, marker='x', linestyle='--', color='r', label='Pivot Random')
@@ -30,9 +30,11 @@ plt.plot(x3, y3, marker='x', linestyle=':', color='b', label='Pivot Median')
 # Add labels and title
 plt.xlabel('Number of Inputs(n)')
 plt.ylabel('Time in Milli Seconds')
-plt.title('Best case running time of QuickSort for all three versions')
+plt.title('Average case running time of QuickSort(Magnified view: Version 2 vs Version 3)')
 plt.legend()
 
+#
+#Worst case running time of QuickSort(Magnified view: Version 2 vs Version 3)
 # Show the plot
 plt.grid(True)
 plt.show()
